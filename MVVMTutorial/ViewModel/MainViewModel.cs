5ccd86a7b3571379622ab779a122acab0b7515ea
ike <ike@DESKTOP-1QOOKR6>
2016-05-12 14:40:45 +0200

    RealyCommand

--- MVVMTutorial/ViewModel/MainViewModel.cs
+++ MVVMTutorial/ViewModel/MainViewModel.cs
@@ -1,6 +1,39 @@
-﻿namespace MVVMTutorial.ViewModel
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+
+namespace MVVMTutorial.ViewModel
 {
-    public class MainViewModel
+    public class MainViewModel : ViewModelBase
     {
+        public string Message
+        {
+            get { return _message; }
+            set { Set(ref _message, value); }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+
+            set
+            {
+                Set(ref _name, value);
+                SayHello.RaiseCanExecuteChanged();
+            }
+        }
+
+        public RelayCommand SayHello
+        {
+            get
+            {
+                return _sayHello ??
+                       (_sayHello =
+                           new RelayCommand(() => { Message = $"Hello {Name}"; }, () => !string.IsNullOrEmpty(Name)));
+            }
+        }
+
+        private string _message;
+        private string _name;
+        private RelayCommand _sayHello;
     }
 }
---
+++ MVVMTutorial/ViewModel/ViewModelLocator.cs
@@ -0,0 +1,7 @@
+﻿namespace MVVMTutorial.ViewModel
+{
+    public class ViewModelLocator
+    {
+        public MainViewModel Main => new MainViewModel();
+    }
+}
